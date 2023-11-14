5952bb61704f39c8d8cb130d100f21fb9ed622bc
Gustavo Cruz <gu.cruz17@hotmail.com>
2023-11-14 07:54:36 -0300

    Modifying UserRepository findById

--- user-service/src/main/java/com/gustavo/userservice/entities/User.java
+++ user-service/src/main/java/com/gustavo/userservice/entities/User.java
@@ -13,6 +13,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -61,7 +62,7 @@ public class User implements Serializable {
 	@Column(nullable = false)
 	private LocalDateTime lastUpdateDate;
 	
-	@ManyToMany()
+	@ManyToMany(fetch = FetchType.LAZY)
 	@JoinTable(
 			name = "TB_USER_ROLE",
 			joinColumns = @JoinColumn(name = "user_id"),
--- user-service/src/main/java/com/gustavo/userservice/repositories/UserRepository.java
+++ user-service/src/main/java/com/gustavo/userservice/repositories/UserRepository.java
@@ -1,7 +1,9 @@
 package com.gustavo.userservice.repositories;
 
+import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.gustavo.userservice.entities.User;
@@ -11,6 +13,14 @@ public interface UserRepository extends JpaRepository<User, UUID> {
 	boolean existsByUsername(String username);
 	
 	boolean existsByEmail(String email);
-
+	
+	// O módulo Spring Data aplicará a estratégia FetchType.EAGER nos nós de 
+	// atributos especificados, mesmo que nossa entidade declare uma estratégia de carregamento LAZY. 
+	// E para outros, será aplicada a estratégia FetchType.LAZY.
+	// https://www.baeldung.com/spring-data-jpa-named-entity-graphs
+	@EntityGraph(
+			attributePaths = "roles", type = EntityGraph.EntityGraphType.FETCH
+	)
+	Optional<User> findById(UUID userId);
 
 }
